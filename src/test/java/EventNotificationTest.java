3510103b529345e6588da34c96a5e20e9e922ddb
matias <matiaseidis@gmail.com>
2013-09-07 02:57:41 -0300

    demo test

---
+++ src/test/java/EventNotificationTest.java
@@ -0,0 +1,23 @@
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+
+public class EventNotificationTest extends TestCase {
+
+	
+	/**
+	 * 1 - prendo el dimon, lo apago
+	 * durante el proceso hago alive's
+	 * despues ver que en el repo esten todos los eventos
+	 * 
+	 * 
+	 * 2 - caso 1 + plan, con update de puller y pushers. 
+	 * Ver que en el repo, en el planMap solo esta el update
+	 * pero en el listado por evento estan todos
+	 */
+	@Test
+	public void testEventNotificationWorks(){
+		
+	}
+}
